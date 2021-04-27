45178f4253162522894322b041cd3e5bc6d2a6d5
Sergey Melnikov <jettpy@yandex.ru>
2021-04-27 21:44:55 +0300

    refactor: изменить карточки мест из коробки

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,13 @@
-let name = document.querySelector('.profile__name');
-let status = document.querySelector('.profile__status');
-let form = document.querySelector('.popup__form');
-let inputName = document.querySelector('.popup__input_field_name');
-let inputStatus = document.querySelector('.popup__input_field_status');
-let popup = document.querySelector('.popup');
-let editButton = document.querySelector('.button_type_edit');
-let closeButton = document.querySelector('.button_type_close');
+const name = document.querySelector('.profile__name');
+const status = document.querySelector('.profile__status');
+const form = document.querySelector('.popup__form');
+const inputName = document.querySelector('.popup__input_field_name');
+const inputStatus = document.querySelector('.popup__input_field_status');
+const popup = document.querySelector('.popup');
+const editButton = document.querySelector('.button_type_edit');
+const closeButton = document.querySelector('.button_type_close');
+const elements = document.querySelector('.elements');
+const elementTemplate = document.querySelector('#element').content;
 
 function refresh() { //функция обновления полей в форме
   inputName.value = name.textContent;
@@ -27,6 +29,41 @@ function saveProfileData(event) { //функция сохранения имен
   status.textContent = inputStatus.value;
   popUpClose();
 }
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+initialCards.forEach(function (card){
+  const cardElement = elementTemplate.querySelector('.element').cloneNode(true);
+  cardElement.querySelector('.element__image').src = card.link;
+  cardElement.querySelector('.element__title').textContent = card.name;
+  elements.append(cardElement);
+});
+
 
 editButton.addEventListener('click', popUpOpen);
 closeButton.addEventListener('click', popUpClose);
